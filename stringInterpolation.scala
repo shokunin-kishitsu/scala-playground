7ffc3380aaefd625780cee7a4b174ee1daff9b13
Oleksiy Holubyev <lyosha@gmail.com>
2016-10-08 15:52:11 +0200

    Added more string interpolations

--- stringInterpolation.scala
+++ stringInterpolation.scala
@@ -1,2 +1,9 @@
 val name = "reader"
 println(s"Hello, $name!")
+
+println(s"The result is ${34 + 23}")
+
+println(raw"No\\\\ escape!!!")
+
+val pi = "Pi"
+println(f"The value of $pi is (roughly) ${math.Pi}%.5f")
