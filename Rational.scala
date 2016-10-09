26fc07d1843259277151e86b4971ceec050d7ea5
Oleksiy Holubyev <lyosha@gmail.com>
2016-10-09 17:19:20 +0200

    Added private greatest common divisor

--- Rational.scala
+++ Rational.scala
@@ -1,17 +1,23 @@
 class Rational(n: Int, d: Int) {
 	require (d != 0)
-	val numerator: Int = n
-	val denominator: Int = d
+
+	private val g = gcd(n.abs, d.abs)
+	val numerator = n / g
+	val denominator = d / g
 
 	def this(n: Int) = this(n, 1)
 
-	override def toString = s"$n/$d"
+	override def toString = s"$numerator/$denominator"
 
 	def add(that: Rational):Rational = {
 		new Rational(
 			numerator * that.denominator + denominator * that.numerator,
 			denominator * that.denominator
 		)
+	}
+
+	private def gcd(a: Int, b: Int): Int = {
+		if (b == 0) a else gcd(b, a % b)
 	}
 }
 
@@ -23,5 +29,5 @@ println(r2)
 
 println(r1 add r2)
 
-val r3 = new Rational(5)
-println(r3)
+println(new Rational(5))
+println(new Rational(66, 42))
