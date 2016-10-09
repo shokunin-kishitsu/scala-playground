713a330f7ce51f93f223aa6007a99c06d6284167
Oleksiy Holubyev <lyosha@gmail.com>
2016-10-09 16:58:48 +0200

    Added add() method -- using fields

--- Rational.scala
+++ Rational.scala
@@ -1,5 +1,22 @@
 class Rational(n: Int, d: Int) {
+	require (d != 0)
+	val numerator: Int = n
+	val denominator: Int = d
+
 	override def toString = s"$n/$d"
+
+	def add(that: Rational):Rational = {
+		new Rational(
+			numerator * that.denominator + denominator * that.numerator,
+			denominator * that.denominator
+		)
+	}
 }
 
-println(new Rational(1, 2))
+val r1 = new Rational(1, 2)
+println(r1)
+
+val r2 = new Rational(44, 467)
+println(r2)
+
+println(r1.add(r2))
