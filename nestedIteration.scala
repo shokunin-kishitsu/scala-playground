aae7bbca9908de898a0df7703c23dd522b2ac345
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-01 22:39:32 +0100

    Added mid-stream variable binding

--- nestedIteration.scala
+++ nestedIteration.scala
@@ -5,8 +5,11 @@ def fileLines(file: java.io.File) =
 
 def grep(pattern: String) =
 	for {
-		file <- filesHere if file.getName.endsWith(".scala")
-		line <- fileLines(file) if line.trim.matches(pattern)
-	} println(file + " : " + line.trim)
+		file <- filesHere
+		if file.getName.endsWith(".scala")
+		line <- fileLines(file)
+		trimmed = line.trim
+		if trimmed.matches(pattern)
+	} println(file + " : " + trimmed)
 
 grep(".*gcd.*")
