fec0595113d9dfbcb5d20249980c6ca6b2a5739c
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-02 08:24:35 +0100

    Match expression now returns a value

--- matchExpressions.scala
+++ matchExpressions.scala
@@ -1,8 +1,10 @@
 val firstArg = if (args.length > 0) args(0) else ""
 
-firstArg match {
-	case "salt" => println("pepper")
-	case "chips" => println("salsa")
-	case "eggs" => println("bacon")
-	case _ => println("wat?")
+var friend = firstArg match {
+	case "salt" => "pepper"
+	case "chips" => "salsa"
+	case "eggs" => "bacon"
+	case _ => "wat?"
 }
+
+println(friend)
