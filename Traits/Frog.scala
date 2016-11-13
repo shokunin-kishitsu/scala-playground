b656afe2a8d6d35d2da2533ad7bd928240bfef08
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-13 22:09:45 +0100

    Frog overridden philosophize() from Philosophical trait

--- Traits/Frog.scala
+++ Traits/Frog.scala
@@ -3,4 +3,5 @@ trait HasLegs
 
 class Frog extends Animal with Philosophical with HasLegs {
 	override def toString() = "green"
+	override def philosophize() = println("It ain't easy being " + toString + "!")
 }
