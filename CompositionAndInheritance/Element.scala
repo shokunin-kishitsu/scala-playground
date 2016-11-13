7fa273484bbaa99a6c741106317545292e0f6a3f
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-13 20:02:29 +0100

    Importing Element.elem and using it in an abstract factory class

--- CompositionAndInheritance/Element.scala
+++ CompositionAndInheritance/Element.scala
@@ -1,10 +1,12 @@
+import Element.elem
+
 abstract class Element {
 	def contents: Array[String]
 	def height: Int = contents.length
 	def width: Int = if (height == 0) 0 else contents(0).length
-	def above(that: Element): Element = new ArrayElement(this.contents ++ that.contents)
+	def above(that: Element): Element = elem(this.contents ++ that.contents)
 	def beside(that: Element): Element =
-		new ArrayElement(
+		elem(
 			for (
 				(line1, line2) <- this.contents zip that.contents
 			) yield line1 + line2
