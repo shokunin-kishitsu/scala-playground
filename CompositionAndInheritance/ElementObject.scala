aae0c3102846f095657c4f34af9705ca9f8dd449
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-13 20:01:39 +0100

    Added Element singleton object

---
+++ CompositionAndInheritance/ElementObject.scala
@@ -0,0 +1,10 @@
+object Element {
+	def elem(contents: Array[String]): Element =
+		new ArrayElement(contents)
+
+	def elem(chr: Char, width: Int, height: Int): Element =
+		new UniformElement(chr, width, height)
+
+	def elem(line: String): Element =
+		new LineElement(line)
+}
