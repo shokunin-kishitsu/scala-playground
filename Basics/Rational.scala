08c4bd912d5935692e977f6dbd7123f7c52f14d2
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-15 00:07:19 +0100

    Mixed in Ordered[Rational] trait into Rational class

--- Basics/Rational.scala
+++ Basics/Rational.scala
@@ -1,4 +1,4 @@
-class Rational(n: Int, d: Int) {
+class Rational(n: Int, d: Int) extends Ordered[Rational] {
 	require (d != 0)
 
 	private val g = gcd(n.abs, d.abs)
@@ -40,6 +40,9 @@ class Rational(n: Int, d: Int) {
   def / (i: Int): Rational =
     new Rational(numerator, denominator * i)
 
+  def compare(that: Rational) =
+		(this.numerator * that.denominator) -  (that.numerator * this.denominator)
+
 	private def gcd(a: Int, b: Int): Int = {
 		if (b == 0) a else gcd(b, a % b)
 	}
@@ -64,3 +67,6 @@ implicit def intToRational(x: Int) = new Rational(x)
 
 println(r2 * 5)
 println(5 * r2)
+
+println(r1 > r2)
+println(r1 <= r2)
