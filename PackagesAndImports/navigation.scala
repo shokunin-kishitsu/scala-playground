381b68ab237e887387cf26e1981900c08dcc3f5d
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-17 20:06:33 +0100

    Beautiful nesting + it compiles

--- PackagesAndImports/navigation.scala
+++ PackagesAndImports/navigation.scala
@@ -1,10 +1,8 @@
-package bobsrockets {
-	class Ship
+package bobsrockets
+package fleets
+class Ship
+class Fleet {
+	def addShip() = { new Ship }
 }
 
-package bobsrockets.fleets {
-	class Fleet {
-		def addShip() = { new Ship }
-	}
-}
 
