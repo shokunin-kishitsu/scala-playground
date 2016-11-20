36961d1a0247f27d8391f8d324f8d5556401ff39
Oleksiy Holubyev <lyosha@gmail.com>
2016-11-20 18:44:19 +0100

    Added more test cases

--- AssertionsAndTests/src/test/scala/ElementSuite.scala
+++ AssertionsAndTests/src/test/scala/ElementSuite.scala
@@ -6,4 +6,28 @@ class ElementSuite extends FunSuite {
 		val ele = elem('x', 2, 3)
 		assert(ele.width == 2)
 	}
+
+  test("assert many things") {
+  	val width = 2
+  	assert(width == 2)
+
+  	assert(List(1,2,3,4).contains(3))
+
+  	val ele = elem('x', 2, 3)
+  	assertResult(2) {
+  		ele.width
+  	}
+
+  	assertThrows[IllegalArgumentException] {
+  		elem('x', -2, 3)
+  	}
+  }
+
+  test("assert intercept") {
+  	val caught =
+  		intercept[ArithmeticException] {
+  			1 / 0
+  		}
+  	assert(caught.getMessage == "/ by zero")
+  }
 }
