e877fb9f511d31525eb71a823d30652a9a6e4eaf
Oleksiy Holubyev <lyosha@gmail.com>
2016-12-15 21:47:03 +0100

    Added some simple operations with case classes

--- CaseClassesAndPatternMatching/CaseClasses.scala
+++ CaseClassesAndPatternMatching/CaseClasses.scala
@@ -3,3 +3,13 @@ case class Var(name: String) extends Expr
 case class Number(num: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
 case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
+
+val v = Var("x")
+val op = BinOp("+", Number(1), v)
+
+println(v.name)
+println(op.left)
+println(op)
+println(op.right == Var("x"))
+
+println(op.copy(operator = "-"))
