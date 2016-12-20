db95e775833ddae87afb400b04ec22c071d34f53
Oleksiy Holubyev <lyosha@gmail.com>
2016-12-20 23:07:05 +0100

    Turned Expr into a sealed class

--- CaseClassesAndPatternMatching/CaseClasses.scala
+++ CaseClassesAndPatternMatching/CaseClasses.scala
@@ -1,4 +1,4 @@
-abstract class Expr
+sealed abstract class Expr
 case class Var(name: String) extends Expr
 case class Number(num: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
