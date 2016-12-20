b946a77468d61910aff0acd25abba5ce75a2748f
Oleksiy Holubyev <lyosha@gmail.com>
2016-12-20 23:28:00 +0100

    Added Option type matching in show()

--- CaseClassesAndPatternMatching/optionType.scala
+++ CaseClassesAndPatternMatching/optionType.scala
@@ -1,3 +1,12 @@
 val capitals = Map("France" -> "Paris", "Japan" -> "Tokyo")
+
 println(capitals get "France")
 println(capitals get "North Pole")
+
+def show(x: Option[String]) = x match {
+	case Some(s) => s
+	case None => "?"
+}
+
+println(show(capitals get "Japan"))
+println(show(capitals get "North Pole"))
