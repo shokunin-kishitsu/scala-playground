c511c00306991cc10070f5b2e92bb9a386751a8d
Oleksiy Holubyev <lyosha@gmail.com>
2016-12-28 19:46:32 +0100

    Added explicit types to lists

--- Lists/lists.scala
+++ Lists/lists.scala
@@ -1,9 +1,9 @@
-val fruit = List("apples", "oranges", "pears")
-val nums = List(1, 2, 3, 4)
-val diag3 =
+val fruit: List[String] = List("apples", "oranges", "pears")
+val nums: List[Int] = List(1, 2, 3, 4)
+val diag3: List[List[Int]] =
   List(
     List(1, 0, 0),
     List(0, 1, 0),
     List(0, 0, 1)
   )
-val empty = List()
+val empty: List[Nothing] = List()
