f811462259f2e2e2fbe4c33afde2854540a59189
Oleksiy Holubyev <lyosha@gmail.com>
2016-12-29 10:57:41 +0100

    Added an alternative way to construct lists

--- Lists/lists.scala
+++ Lists/lists.scala
@@ -8,3 +8,11 @@ val diag3: List[List[Int]] =
   )
 val empty: List[Nothing] = List()
 val xs: List[String] = List()
+
+// alternative way to construct lists
+val fruit2 = "apples" :: ("oranges" :: ("pears" :: Nil))
+val nums2 = 1 :: (2 :: (3 :: (4 :: Nil)))
+val diag3_2 = (1 :: (0 :: (0 :: Nil))) ::
+              (0 :: (1 :: (0 :: Nil))) ::
+              (0 :: (0 :: (1 :: Nil))) :: Nil
+val empty2 = Nil
