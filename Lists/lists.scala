6b237358a3e41d82c991a00b1cc845f9a641de9d
Oleksiy Holubyev <lyosha@gmail.com>
2016-12-29 11:06:22 +0100

    Insertion sorting in Scala

--- Lists/lists.scala
+++ Lists/lists.scala
@@ -16,3 +16,15 @@ val diag3_2 = (1 :: (0 :: (0 :: Nil))) ::
               (0 :: (1 :: (0 :: Nil))) ::
               (0 :: (0 :: (1 :: Nil))) :: Nil
 val empty2 = Nil
+
+// insertion sorting in Scala
+def isort(xs: List[Int]): List[Int] =
+  if (xs.isEmpty) Nil
+  else insert(xs.head, isort(xs.tail))
+
+def insert(x: Int, xs: List[Int]): List[Int] =
+  if (xs.isEmpty || x <= xs.head) x :: xs
+  else xs.head :: insert(x, xs.tail)
+
+
+println(isort(List(5, 6, 2, 8, 3, 1, 0)))
