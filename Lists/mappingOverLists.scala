c20e09e8300feb10960aa8c247ff7b418335aef9
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-02 20:37:39 +0100

    Illustrated difference between map and flatMap

--- Lists/mappingOverLists.scala
+++ Lists/mappingOverLists.scala
@@ -2,3 +2,6 @@ List(1, 2, 3) map (_ + 1)
 val words = List("the", "quick", "brown", "fox")
 words map (_.length)
 words map (_.toList.reverse.mkString)
+
+words map (_.toList)
+words flatMap (_.toList)
