4510e1b5393d8163025de64f92f9fbb4d6415850
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-02 20:58:58 +0100

    Using takeWhile and dropWhile on a list

--- Lists/filteringLists.scala
+++ Lists/filteringLists.scala
@@ -1,3 +1,7 @@
 List.range(1, 6) filter (_ % 2 == 0)
 List.range(1, 6) partition (_ % 2 == 0)
 List.range(1, 6) find (_ % 2 == 0)
+
+List(1, 2, 3, -4, 5) takeWhile (_ > 0)
+val words = List("the", "quick", "brown", "fox")
+words dropWhile (_ startsWith "t")
