00b091695082da268a22b905cf001db13996d4c0
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-05 22:40:58 +0100

    Added and used msortSwapped (with parameters swapped)

--- Lists/mergeSort.scala
+++ Lists/mergeSort.scala
@@ -25,3 +25,23 @@ val reverseSort = msort((x: Int, y: Int) => x > y) _
 reverseSort(List(1, 2, 3, 4, 5, 6, 7, 8))
 
 msort[Int](_ > _)(List(5, 3, 7, 1, 8))
+
+def msortSwapped[T](xs: List[T])(less: (T, T) => Boolean): List[T] = {
+	def merge(xs: List[T], ys: List[T]): List[T] =
+		(xs, ys) match {
+			case (Nil, _) => ys
+			case (_, Nil) => xs
+			case (x :: xs1, y :: ys1) =>
+				if (less(x, y)) x :: merge(xs1, ys)
+				else y :: merge(xs, ys1)
+		}
+
+	val n = xs.length / 2
+	if (n == 0) xs
+	else {
+		val (ys, zs) = xs splitAt n
+		merge(msort(less)(ys), msort(less)(zs))
+	}
+}
+
+msortSwapped(List(5, 3, 7, 1, 8))(_ > _)
