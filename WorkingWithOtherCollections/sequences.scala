4e5c12076140cefd8c07f1bfc52ebd388d7b4db4
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-10 20:14:33 +0100

    Added arrays

--- WorkingWithOtherCollections/sequences.scala
+++ WorkingWithOtherCollections/sequences.scala
@@ -2,3 +2,10 @@
 val colors = List("red", "blue", "green")
 colors.head
 colors.tail
+
+// arrays
+val fiveInts = new Array[Int](5)
+val fiveToOne = Array(5, 4, 3, 2, 1)
+
+fiveInts(0) = fiveToOne(4)
+fiveInts
