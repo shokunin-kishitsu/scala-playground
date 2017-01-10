c952ffc30f94c8e554862e45b8f273122e3b2b64
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-10 20:57:13 +0100

    Various operations on the sets

--- WorkingWithOtherCollections/setsAndMaps.scala
+++ WorkingWithOtherCollections/setsAndMaps.scala
@@ -8,3 +8,15 @@ val words = mutable.Set.empty[String]
 for (word <- wordsArray)
   words += word.toLowerCase
 words
+
+val nums = Set(1, 2, 3)
+nums + 5
+nums - 3
+nums ++ List(5, 6)
+nums -- List(1, 2)
+nums & Set(1, 3, 5, 7)
+nums.size
+nums.contains(3)
+
+words.clear
+words
