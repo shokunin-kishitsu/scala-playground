41bfa091ff3e047eb421e8aa3e7ae7c32d6594f3
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-10 20:51:35 +0100

    Counting distinct words in a sentence

--- WorkingWithOtherCollections/setsAndMaps.scala
+++ WorkingWithOtherCollections/setsAndMaps.scala
@@ -1,3 +1,10 @@
 import scala.collection.mutable
 
 val mutaSet = mutable.Set(1, 2, 3)
+
+val text = "See Spot run. Run, Spot. Run!"
+val wordsArray = text.split("[ !,.]+")
+val words = mutable.Set.empty[String]
+for (word <- wordsArray)
+  words += word.toLowerCase
+words
