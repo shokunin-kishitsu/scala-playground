00ad6d8c10cd03f2947227239085903754454df3
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-12 20:37:16 +0100

    Returning multiple values from a method using tuples

---
+++ WorkingWithOtherCollections/tuples.scala
@@ -0,0 +1,12 @@
+def longestWord(words: Array[String]) = {
+  var word = words(0)
+  var idx = 0
+  for (i <- 1 until words.length)
+    if (words(i).length > word.length) {
+      word = words(i)
+      idx = i
+    }
+  (word, idx)
+}
+
+val longest = longestWord("The quick brown fox".split(" "))
