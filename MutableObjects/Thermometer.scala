e4c42ae643d70b067e2c97bd9ade51f22e575005
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-12 21:11:47 +0100

    Defined a getter and setter without an associate field

---
+++ MutableObjects/Thermometer.scala
@@ -0,0 +1,12 @@
+class Thermometer {
+  var celsius: Float = _
+  def fahrenheit = celsius * 9 / 5 + 32
+  def fahrenheit_= (f: Float) = {
+    celsius = (f - 32) * 5 / 9
+  }
+  override def toString = fahrenheit + "F/" + celsius + "C"
+}
+
+val thermometer = new Thermometer
+thermometer.celsius = 5
+thermometer.fahrenheit
