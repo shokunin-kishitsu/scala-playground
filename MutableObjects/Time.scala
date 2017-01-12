0e625b2b618a5a73732ab2dc42dac10997a15620
Oleksiy Holubyev <lyosha@gmail.com>
2017-01-12 20:59:40 +0100

    Defining getter and setter methods directly on Time class

---
+++ MutableObjects/Time.scala
@@ -0,0 +1,17 @@
+class Time {
+  private[this] var h = 12
+  private[this] var m = 0
+  def hour: Int = h
+  def hour_= (x: Int) = {
+    require(0 <= x && x < 24)
+    h=x
+  }
+  def minute = m
+  def minute_= (x: Int) = {
+    require(0 <= x && x < 60)
+    m=x
+  }
+}
+
+var time = new Time
+time.hour = 25
